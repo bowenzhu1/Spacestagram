ba04bfbc7aff6e1a4fdf24b031d08e988a820bd2
Bowen Zhu <43287170+bowenzhu1@users.noreply.github.com>
2022-01-17 03:19:50 -0500

    update sizing and header

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,7 +4,7 @@ import "./TopBar.css";
 function TopBar() {
   return (
     <div className="topbar">
-      <h1 className="header">Spacestagram</h1>
+      <h1 className="header">🚀Spacestagram</h1>
       <p className="subheader">
         A collection of NASA's Astronomy Photo of the Day
       </p>
